c9f40cc4f6b97ab72f18bab1e489e313b2d71162
azziwa-nwt <azziwa@newwavetech.co.ug>
2015-11-16 08:11:31 -0800

    PATCH: Front-end fix for registration step 1
    Front-end fix for registration step 1 due to deletion by merger.

--- application/views/accounts/register_step_1.php
+++ application/views/accounts/register_step_1.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" type="image/x-icon" href="<?php echo IMAGE_URL;?>favicon.ico">
-  <title><?php echo SITE_TITLE.': Register';?></title>
-
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/external-fonts.css" type="text/css">
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/pss.css" type="text/css" media="screen" />
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/pss.list.css" type="text/css" media="screen" />
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/pss.shadowbox.css" type="text/css" media="screen" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo IMAGE_URL;?>favicon.ico">
+	<title><?php echo SITE_TITLE.': Register';?></title>
+    
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/external-fonts.css" type="text/css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/pss.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/pss.list.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/pss.shadowbox.css" type="text/css" media="screen" />
 </head>
 
 <body>
 <table class='body-table water-mark-bg'>
-
 <?php $this->load->view('addons/public_header');
 
 $this->load->view('addons/public_top_menu', array('__page'=>'register'));
@@ -50,13 +49,26 @@ $this->load->view('addons/public_top_menu', array('__page'=>'register'));
     
     
     
-
     </td>
-    <td>&nbsp;</td>
-  </tr>
+    <td style='vertical-align:top; width:20%;'><span class='bold'>NOTE:</span>
+    <br />
+    Any saved applications will
+be deleted if not submitted within 30 days.
+<br /><br />
+If you have questions on how 
+to fill this form, see our 
+<a href='javascript:;' class='blue'>help section</a> or <a href='javascript:;' class='blue'>drop us a
+message</a>.</td>
+  </tr></table>
+  
+  
+  
+  </td>
+  <td>&nbsp;</td>
+</tr>
 
 
-  <?php $this->load->view('addons/public_footer');?>
+<?php $this->load->view('addons/public_footer');?>
 
 </table>
 <?php echo minify_js('home', array('jquery-2.1.1.min.js', 'jquery-ui.js', 'jquery.form.js', 'pss.js', 'pss.shadowbox.js', 'pss.fileform.js'));?>
